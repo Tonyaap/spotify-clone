578808822c63be49ec64484b96c4afced50b308b
Tonyaap <s.kniest@thesio.eu>
2022-02-05 17:40:24 +0100

    setup login route, create side-panel  1:40

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Sidebar from '../components/Sidebar';
+
+export default function Home() {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+        <main>
+          <Sidebar />
+          {/* center */}
+        </main>
+        <div>
+          {/* //player */}
+        </div>
+    </div>
+  )
+}
